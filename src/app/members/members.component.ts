236bc53773037813913dd39f19b34c973251f7cb
Taisei <taisei.960420@gmail.com>
2024-11-14 09:22:36 +0900

    コンポーネントの作成

---
+++ src/app/members/members.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-members',
+  standalone: true,
+  imports: [],
+  templateUrl: './members.component.html',
+  styleUrl: './members.component.css'
+})
+export class MembersComponent {
+  member = '田中太郎'
+
+}
