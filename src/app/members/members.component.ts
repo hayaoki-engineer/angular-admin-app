caef0352b783475a8cb8ec3aded0a2b3e4022cab
Taisei <taisei.960420@gmail.com>
2024-11-18 08:47:51 +0900

    社員データの一覧表示

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { Member } from '../member';
+import { MEMBERS } from '../mock-members';
+
 @Component({
   selector: 'app-members',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+  ],
   templateUrl: './members.component.html',
   styleUrl: './members.component.css'
 })
 export class MembersComponent {
+  members = MEMBERS;
   member: Member = {
     id: 1,
     name: '田中太郎'
