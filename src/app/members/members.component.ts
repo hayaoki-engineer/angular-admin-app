49fdf383349c77e9070c73ec85505af100a4a0c5
Taisei <taisei.960420@gmail.com>
2024-11-19 08:15:57 +0900

    社員選択時のメッセージログを取得

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { Member } from '../member';
 import { MemberDetailComponent } from '../member-detail/member-detail.component';
 import { MemberService } from '../member.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-members',
@@ -21,7 +22,10 @@ export class MembersComponent {
   members: Member[] = [];
   selectedMember?: Member;
 
-  constructor(private memberService: MemberService) { }
+  constructor(
+    private memberService: MemberService,
+    private messageService: MessageService
+  ) { }
 
   ngOnInit(): void {
     this.getMembers();
@@ -29,6 +33,7 @@ export class MembersComponent {
 
   onSelect(member: Member): void {
     this.selectedMember = member;
+    this.messageService.add(`MembersComponent: 社員データ(id=${member.id})が選択されました`);
   }
 
   getMembers(): void {
